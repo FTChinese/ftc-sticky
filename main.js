f790443bf4039ed3dd79607e086451b5049576ef
weiguo.ni <neefrankie@gmail.com>
2016-03-17 16:53:28 +0800

    add bower and npm

---
+++ main.js
@@ -0,0 +1,43 @@
+function Sticky(fixedEl, startDistance, endDistance) {
+	const oStikcy = this;
+	var rAF = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame || function(callback){ window.setTimeout(callback, 1000/60) }
+
+
+	function init() {	
+		oStikcy.lastPosition = -1;
+		if (!startDistance) {
+			startDistance = 0;
+		}
+		oStikcy.start = startDistance;
+		oStikcy.end = endDistance;
+		if (!(fixedEl instanceof HTMLElement)) {
+			fixedEl = document.querySelector(fixedEl);
+		}
+		oStikcy.fixedEl = fixedEl;
+	}
+
+	function loop(){
+	    // Avoid calculations if not needed
+	    if (oStikcy.lastPosition == window.scrollY) {
+	    	console.log(oStikcy.lastPosition);
+	        rAF(loop);
+	        return false;
+	    } else {
+	    	oStikcy.lastPosition = window.scrollY;
+	    }
+
+	    var withinRange = oStikcy.end ? ((oStikcy.lastPosition > oStikcy.start) && (oStikcy.lastPosition < oStikcy.end)) : (oStikcy.lastPosition > oStikcy.start);
+
+	    if (withinRange) {
+	    	oStikcy.fixedEl.classList.add('o-sticky--fixed');
+	    } else {
+	    	oStikcy.fixedEl.classList.remove('o-sticky--fixed');
+	    }
+
+	    rAF( loop );
+	}
+	init();
+	loop();
+}
+
+module.exports = Sticky;
