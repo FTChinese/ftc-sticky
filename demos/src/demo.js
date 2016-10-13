5fbc96a526e668aaca369eb11a4ff727a6aaaa1b
weiguo.ni <neefrankie@gmail.com>
2016-10-13 13:42:15 +0800

    finish demos

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -1,1 +1,10 @@
 import Sticky from '../../main.js';
+
+const stickyNav = new Sticky('.o-nav');
+
+console.log(stickyNav);
+
+const stickyAside = new Sticky('.secondary-column__inner', {
+	start: 50
+});
+console.log(stickyAside)
